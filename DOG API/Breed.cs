0a2b3ba69773e6e5c77ebb2fa2babf610424ee5f
Thomas Foldal Jakobsen <thom196c@edu.sde.dk>
2022-08-30 15:31:17 +0200

    untested json file reader maybe finished

---
+++ DOG API/Breed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace DOG_API
+{
+    public class Breed
+    {
+        public string breed;
+        public string[] subBreeds;
+        public Breed(string _breed)
+        {
+            breed = _breed;
+        }
+        public Breed(string _breed, string[] _subBreeds)
+        {
+            breed = _breed;
+            subBreeds = _subBreeds;
+        }
+    }
+}
--- DOG API/MainWindow.xaml.cs
+++ DOG API/MainWindow.xaml.cs
@@ -47,7 +47,6 @@ namespace DOG_API
                     throw;
                 }
             }
-            
 
             url = "https://dog.ceo/api/breeds/list/all";
             httpRequest = (HttpWebRequest)WebRequest.Create(url);
@@ -58,15 +57,22 @@ namespace DOG_API
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 var result = streamReader.ReadToEnd();
-                List_Data? data = JsonConvert.DeserializeObject<List_Data>(result);
-                try
-                {
-                    breed_CB.ItemsSource = data!.Message;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                //List<string> breeds = new List<string>();
+                //using (var reader = new JsonTextReader(new StringReader(result)))
+                //{
+                //    while (reader.Read())
+                //    {
+                //        if (reader.Value != null)
+                //        {
+                //            if (reader.Value.ToString() != "message" || reader.Value.ToString() != "status" || reader.Value.ToString() != "success")
+                //            {
+                //                breeds.Add(reader.Value.ToString());
+                //            }
+                //        }
+                //    }
+                //}
+                //breed_CB.ItemsSource = breeds;
+                JsonRead(result);
             }
         }
         private void new_button_Click(object sender, RoutedEventArgs e)
@@ -74,6 +80,29 @@ namespace DOG_API
             var breed = breed_CB.SelectionBoxItem;
             var url = "https://dog.ceo/api/breeds/" + breed + "/images/random";
 
+        }
+        public static List<Breed> JsonRead(string input)
+        {
+            List<string> split = input.Split('{', '}',StringSplitOptions.TrimEntries).ToList();
+            foreach (string text in split)
+            {
+                if (text.Contains("message") || text.Contains("status"))
+                {
+                    split.Remove(text);
+                }
+            }
+            List<string> breeds = split[0].Split('[', ']',StringSplitOptions.None).ToList();
+            Dictionary<string, string> test = new Dictionary<string, string>();
+            List<Breed> options = new List<Breed>();
+            for (int i = 0; i < breeds.Count(); i+=2)
+            {
+                test.Add(breeds[i], breeds[i + 1]);
+            }
+            foreach (KeyValuePair<string,string> j in test)
+            {
+                options.Add(new Breed(j.Key, j.Value.Split(',',StringSplitOptions.RemoveEmptyEntries)));
+            }
+            return options;
         }
     }
 }
